773021c28c29fe5cf82f27b050d1fba141c49237
cguijosa <cindyguijosa@cindys-air.myfiosgateway.com>
2025-03-07 22:56:07 -0500

    Changing to redirect to wedding site

--- src/components/Intropic.js
+++ src/components/Intropic.js
@@ -24,7 +24,7 @@ function IntroPic(){
                 <div className="loading">
                     <h2>N+C</h2>
                 </div>
-                <div className='intro-container'>
+                /*<div className='intro-container'>
                     
                     <div className="cplusn logo">N+C</div>
                     <img style={click?{hidden:true} : {hidden:false}} src={covpic}/>
@@ -32,13 +32,13 @@ function IntroPic(){
                     <div className='icon' onClick={handleClick}>
                         <i><FontAwesomeIcon icon={click ? faPause : faPlay} /></i>
                     </div>
-                    <div className="info"><div className="invite">ENGAGEMENT</div><br></br>Celebrate With us<br></br>4 pm 11.02.2024<br></br><br></br>Cocktail Attire<br></br>Parking in Retail Garage (in front of starbucks)<br></br>Small bites and libations served<br></br>
-                    <br></br>120 Paramount Park Dr Gatihersburg MD 20879<br></br><br></br>
+                    <div className="info"><div className="invite">ENGAGEMENT</div><br></br>Celebrate With us<br></br>4 pm 11.02.2024<br></br>Cocktail Attire<br></br>Parking in Retail Garage (in front of starbucks)<br></br>Small bites and libations served<br></br>
+                    120 Paramount Park Dr Gatihersburg MD 20879<br></br><br></br>
                         <a href="https://nicandcindy.anrsvp.com/#home" target="_blank">
                             <button>RSVP</button>
                         </a>
                     </div>
-                </div>
+                </div>*/
             </body>
         </div>
     );
