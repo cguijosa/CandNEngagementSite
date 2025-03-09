80f40fce2f27ba9946eaec7e54a7ba636e478eb0
cguijosa <cindyguijosa@cindys-air.myfiosgateway.com>
2025-03-08 22:52:09 -0500

    Adding to website url

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' exact Component={Home} />
+        <Route path='/reception' exact Component={Home} />
       </Routes>
     </Router>
   );
